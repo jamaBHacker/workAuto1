c5e18c0b4168b31fb3fa20ca18424c83a5ba65c0
Jamal Butcher <jbutcher@NPI.LOCAL>
2018-04-28 22:31:29 -0400

    v1

---
+++ test.js
@@ -0,0 +1,79 @@
+var fs = require('fs');
+
+var month = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+
+
+function getFormattedDate(today) 
+{
+    var week = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    var day  = week[today.getDay()];
+    var dd   = today.getDate();
+    var mm   = month[today.getMonth()]; //January is 0!
+    var yyyy = today.getFullYear();
+    var hour = today.getHours();
+    var minu = today.getMinutes();
+
+    if(dd<10)  { dd='0'+dd } 
+    if(mm<10)  { mm='0'+mm } 
+    if(minu<10){ minu='0'+minu } 
+
+    return mm + " " + dd + " " + yyyy;
+}
+
+var datetime = new Date();
+var stringDate = getFormattedDate(datetime);
+
+var preDirTag = "C:/Users/PUBLIC/"
+var starElectronicsFolder = preDirTag + "Star Electronics/" + month[datetime.getMonth()];
+var icblFolder = preDirTag + "ICBL Shift Notes/" + month[datetime.getMonth()];
+var thirdEyeFolder = preDirTag + "Third Eye Security Shift Notes/" + month[datetime.getMonth()];
+var cssCms = preDirTag + "CSS CMS Shift Notes/" + month[datetime.getMonth()];
+
+
+var dirs = [starElectronicsFolder, icblFolder, thirdEyeFolder, cssCms];
+
+function createMonthFolder(){
+	starElectronicsFolder += month[datetime.getMonth()];
+	icblFolder = preDirTag += month[datetime.getMonth()];
+	thirdEyeFolder = preDirTag += month[datetime.getMonth()];
+	cssCms += month[datetime.getMonth()];
+	
+	try{
+	dirs.forEach(function(dir) {
+		if(!fs.existsSync(dir)) {
+			fs.mkdirSync(dir);
+		}
+	});
+	console.log("Success");
+}
+
+catch(err) {
+	if(err.code == 'EEXIST') {
+		console.log("Error");
+	}
+
+	else {
+		console.log(err);
+	}
+}
+}
+
+
+try{
+	dirs.forEach(function(dir) {
+		if(!fs.existsSync(dir)) {
+			fs.mkdirSync(dir);
+		}
+	});
+	console.log("Success");
+}
+
+catch(err) {
+	if(err.code == 'EEXIST') {
+		console.log("Error");
+	}
+
+	else {
+		console.log(err);
+	}
+}
